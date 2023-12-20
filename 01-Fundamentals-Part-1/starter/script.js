09d51280b842d0b5a471330bd78c698f14e71087
valmar parts <valmarparts@gmail.com>
2023-12-20 20:11:19 +0200

    Initial progress starter code

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,53 @@
+/*
+////////////////////////////////////
+// Linking a JavaScript File
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+////////////////////////////////////
+// Values and Variables
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Valmar';
+let dreamJob = 'Frontend developer';
+let currentJob = 'Service desk engineer';
+
+console.log(
+  'My name is: ' +
+    firstName +
+    ' and my dream job is, ' +
+    dreamJob +
+    ' but currently my position is: ' +
+    currentJob
+);
+
+
+// Number
+let age = 27;
+
+// String
+
+let pet = 'cat';
+
+// Boolean
+
+let isMotivated = true;
+
+// Undefined
+
+let x;
+
+// Null
+
+let nothing = null;
+
+// Symbol
+
+let sym = Symbol('foo');
+
+// BigInt
+
+let bigInt = 1234567890123456789012345678901234567890n;
+
+*/
