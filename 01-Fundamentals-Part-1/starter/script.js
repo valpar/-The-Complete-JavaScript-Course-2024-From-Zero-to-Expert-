8bdc3a96567235c235fa96e500c8e932f845db2f
Valmar Parts <valmarparts@Valmars-MBP.local>
2024-01-28 23:30:33 +0200

    Update Section 2. point 23.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -87,7 +87,7 @@ console.log(now - 1991 > now - 2020); //https://developer.mozilla.org/en-US/docs
 const averageAge = (ageValmar + ageHelena) / 2;
 console.log(ageHelena, ageValmar);
 console.log(averageAge);
-*/
+
 
 const firstName = "Valmar";
 const job = "Service desk engineer";
@@ -109,3 +109,92 @@ const valmarNew = `I'm ${firstName}, a ${
   year - birthYear
 } years old and my job title is ${job}.`;
 console.log(valmarNew);
+
+
+const age = 23;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+  console.log("Valmar can start driving license 🚗");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Valmar is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 1996;
+let century;
+
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
+
+
+//Type conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Valmar")); //NaN = Not a Number
+console.log(typeof NaN); //Not a number is a number type
+
+console.log(String(23), 23);
+
+//Type coercion
+console.log("I am " + 27 + " years old"); //Type coercion is the automatic or implicit conversion of values from one data type to another (such as strings to numbers).
+console.log("23" - "10" - 3); //10
+console.log("23" + "10" + 3); //23103
+console.log("23" * "2"); //46
+console.log("23" / "2"); //11.5
+console.log("23" > "18"); //true
+
+let n = "1" + 1; //11
+n = n - 1; //10
+console.log(n);
+
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0)); //false
+console.log(Boolean(undefined)); //false
+console.log(Boolean("Valmar")); //true
+console.log(Boolean({})); //true
+console.log(Boolean("")); //false
+
+const money = 0;
+if (money) {
+  //money is a falsy value and if else tries to convert it to a boolean value
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+let height;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
+*/
+
+const age = "18";
+if (age === 18) console.log("You just became an adult :D (strict)");
+
+if (age == 18) console.log("You just became an adult :D (loose)");
+
+const favourite = Number(prompt("What is your favorite number?"));
+console.log(favourite);
+
+if (favourite === 23) {
+  console.log("Cool! 23 is an amazing number!");
+} else if (favourite === 7) {
+  console.log("7 is also a cool number");
+} else if (favourite === 9) {
+  console.log("9 is also a cool number");
+} else {
+  console.log("Number is not 23 or 7");
+}
+
+if (favourite !== 23) console.log("Why not 23?");
