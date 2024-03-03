6775270cd4b81b25104d08e676ba7edbc378353d
valmar parts <valmarparts@gmail.com>
2024-03-03 23:38:28 +0200

    Start section 7 guess my number

---
+++ 05-Guess-My-Number/starter/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 20;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
+let secretNumber = document.querySelector('.number').textContent;
