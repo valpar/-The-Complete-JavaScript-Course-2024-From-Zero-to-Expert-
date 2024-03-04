ded812e86cb5f3cfaa7c0f5ee01b500d6d60acef
valmar parts <valmarparts@gmail.com>
2024-03-04 23:37:18 +0200

    Add click events, game logic and css manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,7 +1,52 @@
 'use strict';
 
+/*
 document.querySelector('.message').textContent = 'Correct Number!';
 document.querySelector('.number').textContent = 20;
 document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 let secretNumber = document.querySelector('.number').textContent;
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  //when no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+
+    //when player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    //when guess too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+    }
+
+    //when guess too low
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      document.querySelector('.score').textContent = score--;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+    }
+  }
+
+  //score
+});
