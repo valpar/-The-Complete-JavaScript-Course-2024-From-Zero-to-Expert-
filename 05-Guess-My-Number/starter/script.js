9e3b88626920667c9be4fb26d82268e941aeb131
valmar parts <valmarparts@gmail.com>
2024-03-05 22:54:41 +0200

    Finish guess my number project

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -9,48 +9,40 @@ let secretNumber = document.querySelector('.number').textContent;
 */
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
-  //when no input
-  if (!guess) {
-    document.querySelector('.message').textContent = 'No number!';
 
-    //when player wins
+  if (!guess) {
+    displayMessage('No number!');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number!';
+    displayMessage('Correct Number!');
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.number').style.width = '30rem';
-
-    //when guess too high
-  } else if (guess > secretNumber) {
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high!';
+      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
-    }
-
-    //when guess too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low!';
-      document.querySelector('.score').textContent = score--;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
+      displayMessage('You lost the game!');
+      document.querySelector('.score').textContent = 0;
     }
   }
-
-  //score
 });
-
 /// coding challenge 1
-
 /*
 Implement a game rest functionality, so that the player can make a new guess! Here is how you can do it:
 
@@ -59,12 +51,11 @@ Implement a game rest functionality, so that the player can make a new guess! He
 3. Restore the initial conditions of the message, number, score and guess input fields
 4. Also restore the original background color (#222) and number width (15rem)
 */
-
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.score').textContent = score;
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
