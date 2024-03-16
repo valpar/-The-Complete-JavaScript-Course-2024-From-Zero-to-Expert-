e2760a57cd5d24eb718c4c65af9daf8e96eb296b
valmar parts <valmarparts@gmail.com>
2024-03-16 22:02:21 +0200

    Finish optional Chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -51,6 +51,26 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+
+//with optional chaining
+console.log(restaurant.openingHours?.mon?.open);
+
+//exmaple
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+//methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+//arrays
+const users = [{ name: 'Mets', email: 'mets@gmail.com' }];
+
+console.log(users[0]?.name ?? 'User array empty');
 
 /*
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
