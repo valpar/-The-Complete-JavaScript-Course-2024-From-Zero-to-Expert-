7abf448dd4509563632add9acafccf972d3d15cf
valmar parts <valmarparts@gmail.com>
2024-03-25 22:27:08 +0200

    Add forEach array looping

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+//const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 /*
@@ -107,7 +107,7 @@ console.log([...arr, ...arr2]);
 
 // JOIN - Does not mutate the original array
 console.log(letters.join(' - '));
-*/
+
 
 const arr = [23, 11, 64];
 console.log(arr[0]);
@@ -120,3 +120,28 @@ console.log(arr.at(-1)); // ES11;
 
 console.log('valmar'.at(0));
 console.log('valmar'.at(-1));
+*/
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+//for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('--------FOREACH--------');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+// 0 : function(200)
+// 1 : function(450)
+// 2 : function(-400)
+// ...
