e7490de2514ffcc83e1462cc5310117ceef6b56b
valmar parts <valmarparts@gmail.com>
2024-03-26 22:59:14 +0200

    Add the map method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -181,3 +181,41 @@ const displayMovements = function (movements) {
 displayMovements(account1.movements);
 
 //console.log(containerMovements.innerHTML);
+
+//Data transformation with Map, filter and reduce
+
+// map returns a new array containing the results of applying an operation on all original array elements
+// filter returns a new array containing the array elements that passed a specified test condition
+// reduce boils down all array elements into a single value (e.g. adding all elements together)
+
+//the map method
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.2; //exchange rate from euros to usd
+
+// const movementsUSEd = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+
+const movementsUSEd = movements.map(movement => movement * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSEd);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+
+console.log(movementDescriptions);
+// if (mov > 0) {
+//   console.log(`Movement ${i + 1}: You deposited ${mov}`);
+// } else {
+//   console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+// }
