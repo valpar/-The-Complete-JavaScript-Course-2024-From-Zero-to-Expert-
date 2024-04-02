aa8785ccc33e30e74c2fb493299ddd775b13acdf
Valmar <valmarparts@gmail.com>
2024-04-02 13:34:40 +0300

    More ways to create and fill arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -383,3 +383,42 @@ movements.sort((a, b) => a - b);
 movements.sort((a, b) => b - a);
 
 //console.log(movements);
+
+console.log([1, 2, 3, 4, 5, 6, 7]);
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+//Empty arrays + fill method
+const x = new Array(7);
+console.log(x);
+//console.log(x.map(() => 5));
+
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+//Array.from
+Array.from({ length: 7 }, () => 1);
+
+const z = Array.from({ length: 7 }, () => 1);
+console.log(z);
+const y = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(y);
+
+const diceRolls = Array.from(
+  { length: 100 },
+  () => Math.trunc(Math.random() * 6) + 1
+);
+console.log(diceRolls);
+
+const movementUI = Array.from(document.querySelectorAll('.movements__value'));
+console.log(movementUI);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+});
