8a17394610b8c38e4a43e2215598c7587f8e60dd
valmar parts <valmarparts@gmail.com>
2024-04-06 19:17:02 +0300

    Add Setters and Getters

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -86,7 +86,6 @@ Array.prototype.unique = function () {
 console.log(arr.unique());
 
 const h1 = document.createElement('h1');
-*/
 
 // ES6 classes
 
@@ -122,3 +121,59 @@ jessica.greet();
 //1. Classes are NOT hoisted
 //2. Classes are first-class citizens
 //3. Classes are executed in strict mode
+*/
+
+//Getters and Setters
+
+const account = {
+  owner: 'Valmar',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2050 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2050 - this.birthYear;
+  }
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const jessica = new PersonCl('Jessica Forest', 2020);
+console.log(jessica);
+console.log(jessica.age);
+
+const walter = new PersonCl('Walter White', 1965);
