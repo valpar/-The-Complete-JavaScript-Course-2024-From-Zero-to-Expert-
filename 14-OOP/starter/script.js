86c0f7b42473b080787004f6e284989285b7d7bf
Valmar <valmarparts@gmail.com>
2024-04-09 15:34:34 +0300

    Add another class example

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -297,7 +297,7 @@ const martha = new StudentCl('Martha Jones', 2001, 'Computer Science');
 console.log(martha);
 martha.introduce();
 martha.calcAge();
-*/
+
 
 // inheritance between classes: Object.create
 
@@ -328,3 +328,50 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 1996, 'Computer Science');
 jay.introduce();
 jay.calcAge();
+*/
+// another class example
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+    }
+  }
+}
+
+const acc1 = new Account('Valmar', 'EUR', 1111);
+console.log(acc1);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+// console.log(acc1);
+
+acc1.deposit(300);
+acc1.withdraw(400);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
