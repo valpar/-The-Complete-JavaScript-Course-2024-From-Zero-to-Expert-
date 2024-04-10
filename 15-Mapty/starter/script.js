0119e54a9a27a14bc0a2f0586d36be1ee237fb89
valmar parts <valmarparts@gmail.com>
2024-04-10 19:30:38 +0300

    Add geolocation API

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -1,16 +1,5 @@
 'use strict';
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
-
 // Project planning
 
 // 1. User stories - description of the app from the user's perspective. All user stories put together describe the entire application.
@@ -41,3 +30,27 @@ const inputElevation = document.querySelector('.form__input--elevation');
 // Flowchart
 
 // Page loads -> Get location -> Load map -> Show map at location -> Click on map -> Show form -> Input data
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      window.latitude = latitude;
+      window.longitude = longitude;
+    },
+    function () {
+      alert('Please enable geolocation!');
+    }
+  );
